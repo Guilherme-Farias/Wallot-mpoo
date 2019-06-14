d122d4e9e99deb050f13805e895ff6e956f19468
Guilherme Farias <guicfarias11@gmail.com>
2019-06-14 00:54:28 -0300

    Wallot limpando antigo cache
    excluindo itens não mais utilizados

--- app/src/main/java/com/ufrpe/wallot_app/infra/WallotAppException.java
+++
@@ -1,6 +0,0 @@
-package com.ufrpe.wallot_app.infra;
-
-public class WallotAppException extends Exception {
-    public WallotAppException(String msg) {super(msg);}
-    public WallotAppException(String msg, Throwable cause) {super(msg, cause);}
-}
