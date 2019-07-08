dd39216cfe344f16ae8ad9fc69b79b487c089278
Augusto Junior <augusto.eu.jr@hotmail.com>
2019-07-07 22:57:56 -0300

    Cleaning

--- app/src/main/java/com/ufrpe/bsi/mpoo/wallotapp/infra/negocio/OnRecyclerListener.java
+++
@@ -1,6 +0,0 @@
-package com.ufrpe.bsi.mpoo.wallotapp.infra.negocio;
-
-public interface OnRecyclerListener {
-    //usada para reconher o click no recyclerView
-    void onClickRecycler(int position);
-}
